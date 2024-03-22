cab03c5f0a558fac9c720faf75d3a7725c96b520
Marcos Irenos de Souza <marcosaurelio.irenos@gmail.com>
2024-03-22 15:21:08 -0300

    Adição menu de navegação
    Adição de dots para a navegação do carrossel de imagens de promoções

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,3 +8,36 @@ function mudarPromo(direction) {
     document.querySelector("#promo").
     src = promos[promoAtual];
 }
+
+const dots = document.querySelectorAll(".dot");
+const setaDireita = document.getElementById("seta-direita");
+const setaEsquerda = document.getElementById("seta-esquerda");
+
+let currentDotIndex = 0;
+
+setaDireita.addEventListener("click", () => {
+    dots[currentDotIndex].classList.remove("active");
+    currentDotIndex = (currentDotIndex + 1) % dots.length;
+    dots[currentDotIndex].classList.add("active");
+});
+
+setaEsquerda.addEventListener("click", () => {
+    dots[currentDotIndex].classList.remove("active");
+    currentDotIndex = (currentDotIndex - 1 + dots.length) % dots.length;
+    dots[currentDotIndex].classList.add("active");
+});
+
+// Seta direita no último dot
+dots[dots.length - 1].addEventListener("click", () => {
+    dots[currentDotIndex].classList.remove("active");
+    currentDotIndex = 0;
+    dots[currentDotIndex].classList.add("active");
+});
+
+// Seta esquerda no primeiro dot
+dots[0].addEventListener("click", () => {
+    dots[currentDotIndex].classList.remove("active");
+    currentDotIndex = dots.length - 1;
+    dots[currentDotIndex].classList.add("active");
+});
+
