f756ad95c9dea59ef82c016d77fa3c917fec3d53
Marcos Irenos de Souza <marcosaurelio.irenos@gmail.com>
2024-03-21 22:56:37 -0300

    Update script.js
    Update de nome das variáveis

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,9 @@
 var promos = ["/Assets/IMG/promo1.jpg", "/Assets/IMG/promo2.jpg"];
-var currentPromo = 0;
+var promoAtual = 0;
 
 function mudarPromo(direction) {
-    currentPromo += direction;
-    if (currentPromo < 0) currentPromo = promos.length - 1;
-    if (currentPromo >= promos.length) currentPromo = 0;
-    document.querySelector("#promo").src = promos[currentPromo];
+    promoAtual += direction;
+    if (promoAtual < 0) promoAtual = promos.length - 1;
+    if (promoAtual >= promos.length) promoAtual = 0;
+    document.querySelector("#promo").src = promos[promoAtual];
 }
