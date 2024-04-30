00ed490b7e46412e62379806b496021cd646f1b2
Marcos Irenos de Souza <marcosaurelio.irenos@gmail.com>
2024-04-30 19:04:04 -0300

    Inserção preço total
    Inserção de valor total quantidade x valor

--- JavaScript/pedidos.js
+++ JavaScript/pedidos.js
@@ -1,11 +1,17 @@
-function calculateTotal() {
-  var priceInput = document.getElementById("price1");
-  var quantityInput = document.getElementById("quantity1");
-  var totalPriceSpan = document.getElementById("total1");
+const priceElement = document.getElementById("price");
+var priceText = priceElement.textContent;
+priceText = priceText.replace(",",".");
+const price = parseInt(priceText);
 
-  var price = parseFloat(priceInput.value);
-  var quantity = parseInt(quantityInput.value);
-  var total = price * quantity;
+const qtdElement = document.getElementById("qtd");
+const qtdText = qtdElement.textContent;
+const qtd = parseInt(qtdText);
 
-  totalPriceSpan.textContent = total.toFixed(2); // Format to two decimal places
-}
+const totalElement = document.getElementById("total");
+
+const result = qtd*price;
+totalElement.textContent = result;
+
+console.log(qtd);
+console.log(price);
+console.log(totalElement);
