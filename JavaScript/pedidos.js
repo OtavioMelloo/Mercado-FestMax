460ba32c742831fa8e74f78437843728390f1340
Marcos Irenos de Souza <marcosaurelio.irenos@gmail.com>
2024-05-01 11:00:03 -0300

    Inclusão de valor total
    Foi incluído o valor total calculado pelo JS, quantidade x valor

--- JavaScript/pedidos.js
+++ JavaScript/pedidos.js
@@ -1,7 +1,6 @@
 const priceElement = document.getElementById("price");
-var priceText = priceElement.textContent;
-priceText = priceText.replace(",",".");
-const price = parseInt(priceText);
+var priceText = priceElement.innerText.replace(",",".");
+const price = parseFloat(priceText);
 
 const qtdElement = document.getElementById("qtd");
 const qtdText = qtdElement.textContent;
@@ -11,7 +10,3 @@ const totalElement = document.getElementById("total");
 
 const result = qtd*price;
 totalElement.textContent = result;
-
-console.log(qtd);
-console.log(price);
-console.log(totalElement);
